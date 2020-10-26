c19c54f28711b3b242fd3f3e7def18c994e1c4c2
Gonzalo Gamez <takosaga@gmail.com>
2020-10-26 13:42:08 -0500

    Finished changes for 179

--- Problem 206/sol1.py
+++ Problem 206/sol1.py
@@ -1,4 +1,4 @@
-import math  
+import math
 
 def solution():
 
@@ -7,33 +7,22 @@ def solution():
     while True:
         add += 2
         total += add
-        
-        if total > 1020304050607080900{
-            str(total)
-            if int(num[-1]) == 0{
-                if int(num[-3]) == 9{
-                    if int(num[-5]) == 8{
-                        if int(num[-7]) == 7{
-                            if int(num[-9]) == 6{
-                                if int(num[-11]) == 5{
-                                    if int(num[-13]) == 4{
-                                        if int(num[-15]) == 3 {
-                                            if int(num[-17]) == 2 {
-                                                if int(num[-19]) == 1:{
+
+        if total > 1020304050607080900:
+            num = str(total)
+            if int(num[-1]) == 0:
+                if int(num[-3]) == 9:
+                    if int(num[-5]) == 8:
+                        if int(num[-7]) == 7:
+                            if int(num[-9]) == 6:
+                                if int(num[-11]) == 5:
+                                    if int(num[-13]) == 4:
+                                        if int(num[-15]) == 3 :
+                                            if int(num[-17]) == 2 :
+                                                if int(num[-19]) == 1:
                                                     break
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }                
 
     return int(math.sqrt(total))
-    
+
 if __name__ == "__main__":
     print(solution())
--- Problem_179/sol1.py
+++ Problem_179/sol1.py
@@ -1,6 +1,27 @@
+"""
+Project Euler Problem 179: https://projecteuler.net/problem=179
+
+Find the number of integers 1 < n < 107, for which n and n + 1 have the same number of positive divisors. For example, 14 has the positive divisors 1, 2, 7, 14 while 15 has 1, 3, 5, 15.
+
+Solution: Finding divisors and than comparing with the following
+number for every number between 1 and 10,000,000
+
+"""
+
 import math
 
 def countDivisors(n) -> int :
+
+    """
+    Counting divisors from:
+    https://www.geeksforgeeks.org/count-divisors-n-on13/?ref=lbp
+
+    >>> countDivisors(10)
+    4
+    >>> countDivisors(11)
+    2
+    """
+
     cnt = 0
     for i in range(1, (int)(math.sqrt(n)) + 1) :
         if (n % i == 0) :
@@ -16,11 +37,15 @@ def countDivisors(n) -> int :
 
 
 def solution() -> int:
+    """
+    >>> solution()
+    986262
 
+    """
     consecutive_positive_divisors = 0
     temp_divisors = 0
 
-    for number in range(2,10000000):
+    for number in range(2,10_000_000):
         divisors = countDivisors(number)
         if divisors == temp_divisors:
             consecutive_positive_divisors += 1
@@ -29,4 +54,4 @@ def solution() -> int:
     return consecutive_positive_divisors
 
 if __name__ == "__main__":
-    print(solution())
+    print(f"{solution() = }")
